1dba2049aa1230e46a136cb7aa8da2bfb385fe8d
damonmaldonado <damon@mpwmarketing.com>
2016-05-13 12:50:07 -0400

    piklist data update branch
    created branch to use after applying the piklist data update branch

--- geopages-pk.php
+++ geopages-pk.php
@@ -3,7 +3,7 @@
 Plugin Name: Editable Geo Pages
 Plugin URI:
 Description: Add new editable geo pages
-Version: 0.3.2
+Version: 0.3.3
 Author: DMM
 Author URI:
 Plugin Type: Piklist
@@ -52,57 +52,75 @@ return $post_types;
 //Create the page content
 function print_it ( $atts ) {
 extract( shortcode_atts( array(
-		'name' => '', 'replacements'=> '', 'choices'=>0
-	), $atts ) );
-	//get the paragraph choices made and decrement the values to matcht he array indices
-	$arr_val = explode("^",$choices);
-if ($arr_val[0]>0) {	
-	foreach ($arr_val as &$val){
-	$val = $val - 1;
+    'name' => '', 'replacements'=> '', 'choices'=>0
+  ), $atts ) );
+  //get the paragraph choices made and decrement the values to matcht he array indices
+  $arr_val = explode("^",$choices);
+if ($arr_val[0]>0) {  
+  foreach ($arr_val as &$val){
+  $val = $val - 1;
 }
 }
-/*print_r ($arr_val);//check choice values */
+//print_r ($arr_val);//check choice values 
 //Get the geo page they chose to use, Send error if page not found
 $page = get_page_by_title( $name , OBJECT , 'geo_page' );
 if ($page =='') {
-	echo 'Please verify that the Geo Page ' . $name . ' exists and that the page name matches your entry exactly';
-	return;
+  echo 'Please verify that the Geo Page ' . $name . ' exists and that the page name matches your entry exactly';
+  return;
 }
 //get list of terms to replace if no terms entered give an error
 $iid = $page->ID;
 $sea = get_post_meta($iid, 'sea_val', true);
+//echo $sea;
 if ($sea == '') {
-	echo 'Please enter a value in the string to replace section for the Geo Page ' . $name;
-	return;
+  echo 'Please enter a value in the string to replace section for the Geo Page ' . $name;
+  return;
 }
 $search_arr =  explode("^", $sea);
 $search_link_arr = $search_arr;
 array_walk($search_link_arr, function (&$value, $key) {
    $value=$value . '-link%';
 });
-
+//print_r($search_arr);
 //Get replacement values from shortcode
 $replace_arr = explode("^", $replacements);
 $replace_link_arr = str_replace(" ","-",$replace_arr);
 $replace_link_arr = str_replace(",","",$replace_link_arr);
-
+//print_r($replace_arr);
 //Get meta options with all possible paragraphs and choose paragraphs based on users choices
 $all_para =  get_post_meta($iid, 'all_paragraphs');
+//echo 'NEWWWWW';
+/*echo '<pre>';
+print_r($all_para[0]);
+echo '</pre>';*/
+$num_sections = count($all_para[0]);
+//echo "NUM ".$num_sections;
 $final_array = array();
-	$para_data_arr = $all_para[0]['paragraph_choices'];
-	$loopstodo = count ($para_data_arr);
-	$choices_picked = count ($arr_val);
-	while ($choices_picked < $loopstodo) {	
-		$arr_val[] = 0;
-		$choices_picked++;
-	}
-	
-	for ($i = 0; $i < $loopstodo; $i++) {
-	    $final_array[]=$para_data_arr[$i]['paragraph_choice'][$arr_val[$i]];
-	}
-	//create string from final choices
-	foreach ($final_array as $val){
-	$raw_cont .= $val;
+//get number of sections and set array to match
+for ($i = 0; $i < $num_sections; $i++){
+  $para_data_arr[] = $all_para[0][$i]['paragraph_choices'];
+}
+/*echo '<pre>';
+print_r($para_data_arr);
+echo '</pre>';*/
+  $loopstodo = count ($para_data_arr);
+  $choices_picked = count ($arr_val);
+  //echo $loopstodo . $choices_picked;
+  while ($choices_picked < $loopstodo) {  
+    $arr_val[] = 0;
+    $choices_picked++;
+  }
+  
+  for ($i = 0; $i < $loopstodo; $i++) {
+    /*echo "NEXT". $arr_val[$i];
+    echo '<pre>';
+    print_r($para_data_arr[$i][$arr_val[$i]]['paragraph_choice']);
+    echo '</pre>';*/
+      $final_array[]=$para_data_arr[$i][$arr_val[$i]]['paragraph_choice'];
+  }
+  //create string from final choices
+  foreach ($final_array as $val){
+  $raw_cont .= $val;
 }
 //combine regular and link search and replace arrays
 $search_arr_final = array_merge ($search_link_arr, $search_arr);
@@ -110,7 +128,9 @@ $replace_arr_final = array_merge ($replace_link_arr, $replace_arr);
 
 // make replacements and return value
 $cont = str_replace($search_arr_final, $replace_arr_final, $raw_cont);
+$cont = stripslashes($cont);
 return do_shortcode($cont);
+
 
 
 }
@@ -157,7 +177,9 @@ $replace_link_arr = str_replace(",","",$replace_link_arr);
 //Get meta options with all possible paragraphs and choose paragraphs based on users choices
 $all_para =  get_post_meta($iid, 'all_paragraphs');
 //echo 'NEWWWWW';
-//print_r($all_para[0]);
+/*echo '<pre>';
+print_r($all_para[0]);
+echo '</pre>';*/
 $num_sections = count($all_para[0]);
 //echo "NUM ".$num_sections;
 $final_array = array();
@@ -165,7 +187,9 @@ $final_array = array();
 for ($i = 0; $i < $num_sections; $i++){
   $para_data_arr[] = $all_para[0][$i]['paragraph_choices'];
 }
-//print_r($para_data_arr);
+/*echo '<pre>';
+print_r($para_data_arr);
+echo '</pre>';*/
   $loopstodo = count ($para_data_arr);
   $choices_picked = count ($arr_val);
   //echo $loopstodo . $choices_picked;
@@ -175,8 +199,10 @@ for ($i = 0; $i < $num_sections; $i++){
   }
   
   for ($i = 0; $i < $loopstodo; $i++) {
-    //echo "NEXT". $arr_val[$i];
-    //print_r($para_data_arr[$i][$arr_val[$i]]['paragraph_choice'][0]);
+   /* echo "NEXT". $arr_val[$i];
+    echo '<pre>';
+    print_r($para_data_arr[$i][$arr_val[$i]]['paragraph_choice'][0]);
+    echo '</pre>';*/
       $final_array[]=$para_data_arr[$i][$arr_val[$i]]['paragraph_choice'][0];
   }
   //create string from final choices
@@ -195,80 +221,3 @@ return do_shortcode($cont);
 
 }
 add_shortcode( 'geopage2', 'print_it2');
-
-
-function test_print_it ( $atts ) {
-extract( shortcode_atts( array(
-    'name' => '', 'replacements'=> '', 'choices'=>0
-  ), $atts ) );
-  //get the paragraph choices made and decrement the values to matcht he array indices
-  $arr_val = explode("^",$choices);
-if ($arr_val[0]>0) {  
-  foreach ($arr_val as &$val){
-  $val = $val - 1;
-}
-}
-//print_r ($arr_val);//check choice values 
-//Get the geo page they chose to use, Send error if page not found
-$page = get_page_by_title( $name , OBJECT , 'geo_page' );
-if ($page =='') {
-  echo 'Please verify that the Geo Page ' . $name . ' exists and that the page name matches your entry exactly';
-  return;
-}
-//get list of terms to replace if no terms entered give an error
-$iid = $page->ID;
-$sea = get_post_meta($iid, 'sea_val', true);
-//echo $sea;
-if ($sea == '') {
-  echo 'Please enter a value in the string to replace section for the Geo Page ' . $name;
-  return;
-}
-$search_arr =  explode("^", $sea);
-$search_link_arr = $search_arr;
-array_walk($search_link_arr, function (&$value, $key) {
-   $value=$value . '-link%';
-});
-//print_r($search_arr);
-//Get replacement values from shortcode
-$replace_arr = explode("^", $replacements);
-$replace_link_arr = str_replace(" ","-",$replace_arr);
-$replace_link_arr = str_replace(",","",$replace_link_arr);
-//print_r($replace_arr);
-//Get meta options with all possible paragraphs and choose paragraphs based on users choices
-$all_para =  get_post_meta($iid, 'all_paragraphs');
-//echo 'NEWWWWW';
-print_r($all_para);
-$num_sections = count($all_para[0]);
-//echo "NUM ".$num_sections;
-$final_array = array();
-//get number of sections and set array to match
-for ($i = 0; $i < $num_sections; $i++){
-  $para_data_arr[] = $all_para[0][$i]['paragraph_choices'];
-}
-//print_r($para_data_arr);
-  $loopstodo = count ($para_data_arr);
-  $choices_picked = count ($arr_val);
-  //echo $loopstodo . $choices_picked;
-  while ($choices_picked < $loopstodo) {  
-    $arr_val[] = 0;
-    $choices_picked++;
-  }
-  
-  for ($i = 0; $i < $loopstodo; $i++) {
-    //echo "NEXT". $arr_val[$i];
-    //print_r($para_data_arr[$i][$arr_val[$i]]['paragraph_choice'][0]);
-      $final_array[]=$para_data_arr[$i][$arr_val[$i]]['paragraph_choice'][0];
-  }
-  //create string from final choices
-  foreach ($final_array as $val){
-  $raw_cont .= $val;
-}
-//combine regular and link search and replace arrays
-$search_arr_final = array_merge ($search_link_arr, $search_arr);
-$replace_arr_final = array_merge ($replace_link_arr, $replace_arr);
-// make replacements and return value
-$cont = str_replace($search_arr_final, $replace_arr_final, $raw_cont);
-$cont = stripslashes($cont);
-return do_shortcode($cont);
-}
-add_shortcode( 'testprint', 'test_print_it');
